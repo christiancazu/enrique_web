21b165e816f941b6bdcdd4fb81a929332876cad2
mariley <mariley2018@gmail.com>
2019-09-23 16:55:36 -0500

    added config eslintr

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,17 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
     node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-    'prettier'
-  ],
-  // add your custom rules here
-  rules: {
   }
 }
