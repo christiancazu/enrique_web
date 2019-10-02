6c8cc19893f8ae31992de65f3752e80e72f981d9
mariley <mariley2018@gmail.com>
2019-10-02 17:11:44 -0500

    resolved conflicts

--- jq-script/jq.carousel.main.js
+++ jq-script/jq.carousel.main.js
@@ -4,11 +4,7 @@ export default {
   },
   methods: {
     loadJQCarouselMain() {
-<<<<<<< HEAD
-      window.jQuery('#carouselMain.carousel').carousel({
-=======
       window.jQuery('#carouselMain').carousel({
->>>>>>> 30ad65f5e8e3beabe94bd0279ac2d130142d7e2a
         interval: 4000
       })
     }
--- jq-script/jq.carousel.testimonial.js
+++ jq-script/jq.carousel.testimonial.js
@@ -4,13 +4,8 @@ export default {
   },
   methods: {
     loadJQCarouselTestimonial() {
-<<<<<<< HEAD
-      window.jQuery('#carouselTestimonial.carousel').carousel({
-        // interval: 1000
-=======
       window.jQuery('#carouselTestimonial').carousel({
         interval: 4000
->>>>>>> 30ad65f5e8e3beabe94bd0279ac2d130142d7e2a
       })
     }
   },
