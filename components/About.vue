fbffa1ede5bb04375a3f097d4e46529d764036aa
Christian Carrillo Zúñiga <christiancazu@gmail.com>
2019-10-02 00:25:02 -0500

    implemented About Component

--- components/About.vue
+++ components/About.vue
@@ -3,74 +3,39 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-12 text-center">
-          <h2 class="section-heading text-uppercase">About</h2>
-          <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+          <h2 class="section-heading text-uppercase my-5">Sobre mi</h2>
+          <h3 class="section-subheading text-muted my-5">Mis actividades</h3>
         </div>
       </div>
       <div class="row">
         <div class="col-lg-12">
           <ul class="timeline">
-            <li>
+            <li
+              v-for="(activity, i) in activities" :key="i"
+              :class="{ 'timeline-inverted': i % 2 }"
+            >
               <div class="timeline-image">
-                <img class="rounded-circle img-fluid" src="images/carousel-main/cropped-viajar-solo.jpg" alt="">
+                <img 
+                  :src="imgPath + activity.imgSmall"
+                  :alt="'Luis Enrique Bustamante Pérez - ' + activity.title"
+                  class="rounded-circle img-fluid" 
+                >
               </div>
               <div class="timeline-panel">
                 <div class="timeline-heading">
-                  <h4>2009-2011</h4>
-                  <h4 class="subheading">Our Humble Beginnings</h4>
+                  <h4>{{ activity.title }}</h4>
+                  <h4 class="subheading">{{ activity.date }}</h4>
                 </div>
                 <div class="timeline-body">
-                  <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                  <p class="text-muted">{{ activity.description }}</p>
                 </div>
               </div>
             </li>
             <li class="timeline-inverted">
               <div class="timeline-image">
-                <img class="rounded-circle img-fluid" src="images/carousel-main/cropped-viajar-solo.jpg" alt="">
-              </div>
-              <div class="timeline-panel">
-                <div class="timeline-heading">
-                  <h4>March 2011</h4>
-                  <h4 class="subheading">An Agency is Born</h4>
-                </div>
-                <div class="timeline-body">
-                  <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
-                </div>
-              </div>
-            </li>
-            <li>
-              <div class="timeline-image">
-                <img class="rounded-circle img-fluid" src="images/carousel-main/cropped-viajar-solo.jpg" alt="">
-              </div>
-              <div class="timeline-panel">
-                <div class="timeline-heading">
-                  <h4>December 2012</h4>
-                  <h4 class="subheading">Transition to Full Service</h4>
-                </div>
-                <div class="timeline-body">
-                  <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
-                </div>
-              </div>
-            </li>
-            <li class="timeline-inverted">
-              <div class="timeline-image">
-                <img class="rounded-circle img-fluid" src="images/carousel-main/cropped-viajar-solo.jpg" alt="">
-              </div>
-              <div class="timeline-panel">
-                <div class="timeline-heading">
-                  <h4>July 2014</h4>
-                  <h4 class="subheading">Phase Two Expansion</h4>
-                </div>
-                <div class="timeline-body">
-                  <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
-                </div>
-              </div>
-            </li>
-            <li class="timeline-inverted">
-              <div class="timeline-image">
-                <h4>Be Part
-                  <br>Of Our
-                  <br>Story!</h4>
+                <h4>
+                  <br>Ver más
+                </h4>
               </div>
             </li>
           </ul>
@@ -81,7 +46,41 @@
 </template>
 
 <script>
+import constants from '~/config/constants'
+
 export default {
-name: 'About'
+  name: 'About',
+  data() {
+    return {
+      imgPath: constants.IMG_BASE_PATH + '/about/',
+      activities: [    
+        {
+          title: 'Medalla de Honor de la UNAP',
+          date: 'Diciembre 20, 2017',
+          description: 'La Universidad Nacional del altiplano otorga Medalla de Honor a Luis Enrique Bustamante Pérez.',
+          link: '',
+          imgSmall: 'diciembre-20-2017-1-200x200.jpg',
+          imgBig: 'diciembre-20-2017-1.jpg'
+        },   
+        {
+          title: 'Reconocimiento en Ayaviri',
+          date: 'Diciembre 21, 2017',
+          description: 'Luis Enrique recibe la Medalla de Honor por parte de la Municipalidad Provincial de Ayaviri.',
+          link: '',
+          imgSmall: 'diciembre-21-2017-1-200x200.jpg',
+          imgBig: 'diciembre-21-2017-1.jpg'
+        },
+        {
+          title: 'Joven arequipeño dio emotivo discurso utilizando un puntero y una computadora',
+          date: 'Octubre 28, 2018',
+          description: 'Luis Enrique Bustamante Perez (32) fue hoy el protagonista de un momento emotivo en el compartir de personas con discapacidad que se llevó a cabo ayer en el comedor de la Universidad Nacional de San Agustín, donde estuvo a cargo del discurso central.',
+          link: '',
+          imgSmall: 'octubre-28-2018-1-200x200.jpg',
+          imgBig: 'octubre-28-2018-1.jpg'
+        }
+      ]
+    }
+  }
+
 }
 </script>
