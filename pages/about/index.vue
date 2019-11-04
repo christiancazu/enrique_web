00c9284cb04415b5f081410bc563bbfb4bb75a27
mariley <mariley2018@gmail.com>
2019-11-04 17:20:11 -0500

    advanced about me

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,6 +1,55 @@
 <template>
   <div class="container about-page">
-    <header-section
+    <ul
+      id="tabsAbout"
+      class="nav nav-pills nav-justified"
+      role="tablist"
+    >
+      <li class="nav-item">
+        <a
+          id="home-tab"
+          class="nav-link active h6 mb-0"
+          data-toggle="tab"
+          href="#home"
+          role="tab"
+          aria-controls="home"
+          aria-selected="true"
+        >Biografía</a>
+      </li>
+      <li class="nav-item">
+        <a
+          id="profile-tab"
+          class="nav-link h6 mb-0"
+          data-toggle="tab"
+          href="#profile"
+          role="tab"
+          aria-controls="profile"
+          aria-selected="false"
+        >Cartas</a>
+      </li>
+    </ul>
+    <div
+      id="myTabContent"
+      class="tab-content"
+    >
+      <div
+        id="home"
+        class="tab-pane fade show active"
+        role="tabpanel"
+        aria-labelledby="home-tab"
+      >
+        ...
+      </div>
+      <div
+        id="profile"
+        class="tab-pane fade"
+        role="tabpanel"
+        aria-labelledby="profile-tab"
+      >
+        ...
+      </div>
+    </div>
+    <!-- <header-section
       :attach="header.attach"
       :title="header.title"
       :description="header.description"
@@ -15,18 +64,18 @@
       :body="about.body"
       :footer="about.footer"
       :reverse="!!(i % 2)"
-    />
+    /> -->
   </div>
 </template>
 
 <script>
-import HeaderSection from '~/components/HeaderSection'
-import History from '~/components/History'
+// import HeaderSection from '~/components/HeaderSection'
+// import History from '~/components/History'
 import { IMG_BASE_PATH } from '~/config/constants'
 import { mapState } from 'vuex'
 export default {
   components: {
-    HeaderSection, History
+    // HeaderSection, History
   },
   data () {
     return {
