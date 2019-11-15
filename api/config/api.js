6efe84f4fceb42c03d7eb1b7483c576bfa279a02
mariley <mariley2018@gmail.com>
2019-11-15 17:07:51 -0500

    page contact

--- api/config/api.js
+++ api/config/api.js
@@ -1,39 +1,39 @@
-function api($axios) {
-    return {
-        post(url, payload = {}) {
-            return $axios({
-                url: `${url}/`,
-                method: 'POST',
-                data: payload.data || {}
-            })
-        },
-        put(url, pk, payload = {}) {
-            return $axios({
-                url: `${url}/${pk}/`,
-                method: 'PUT',
-                data: payload.data || {}
-            })
-        },
-        delete(url, pk) {
-            return $axios({
-                url: `${url}/${pk}/`,
-                method: 'DELETE'
-            })
-        },
-        get(url, payload = {}) {
-            return $axios({
-                url: `${url}/`,
-                method: 'GET',
-                data: payload.data || {}
-            })
-        },
-        getById(url, pk) {
-            return $axios({
-                url: `${url}/${pk}/`,
-                method: 'GET',
-            })
-        },
-    }
+function api ($axios) {
+  return {
+    post (url, payload = {}) {
+      return $axios({
+        url: `${url}/`,
+        method: 'POST',
+        data: payload.data || {}
+      })
+    },
+    put (url, pk, payload = {}) {
+      return $axios({
+        url: `${url}/${pk}/`,
+        method: 'PUT',
+        data: payload.data || {}
+      })
+    },
+    delete (url, pk) {
+      return $axios({
+        url: `${url}/${pk}/`,
+        method: 'DELETE'
+      })
+    },
+    get (url, payload = {}) {
+      return $axios({
+        url: `${url}/`,
+        method: 'GET',
+        data: payload.data || {}
+      })
+    },
+    getById (url, pk) {
+      return $axios({
+        url: `${url}/${pk}/`,
+        method: 'GET',
+      })
+    },
+  }
 }
 
 export { api }
